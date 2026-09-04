bf18e7adee9881279dbcdaf65c83a7a7e9b5a8f0
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 02:53:53 +0900

    ✨ feat(core): Activity에 ActivityId 값 객체 추가
    Activity를 식별하기 위한 ActivityId를 Activity 도메인
    모델의 정적 중첩 클래스로 추가한다. Lombok @Value를 사용해
    불변 값 객체로 구현하여 식별자의 타입 안정성을 높인다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
@@ -1,4 +1,13 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
+import lombok.Value;
+
 public class Activity {
+    ActivityId id;
+
+
+    @Value
+    public static class ActivityId {
+        Long value;
+    }
 }
