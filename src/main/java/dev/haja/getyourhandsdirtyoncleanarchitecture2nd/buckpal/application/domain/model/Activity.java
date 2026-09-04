34862d421d4707967beeb1a1af19e209caa9d8bf
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 03:00:52 +0900

    ✨ feat(core): Activity에 출금/입금 계좌, 시각, 금액 필드 추가
    계좌 이체 내역을 온전히 표현하도록 sourceAccountId,
    targetAccountId, timestamp, money 필드를 추가한다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
@@ -3,10 +3,18 @@ package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.dom
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 public class Activity {
     ActivityId id;
     @Nonnull Account.AccountId ownerAccountId;
+    @Nonnull Account.AccountId sourceAccountId;
+    @Nonnull Account.AccountId targetAccountId;
+    @Nonnull LocalDateTime timestamp;
+    @Nonnull Money money;
+
 
     @Value
     public static class ActivityId {
