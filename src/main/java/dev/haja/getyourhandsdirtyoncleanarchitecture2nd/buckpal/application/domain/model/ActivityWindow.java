1f524f6be0b405ae9a505ba50b38a6d9b3d9695f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 02:55:45 +0900

    ✨ feat(core): ActivityWindow에 생성자 추가
    List와 가변 인자(varargs) 두 가지 형태로 Activity를 받아
    ActivityWindow를 생성할 수 있는 생성자를 추가한다. @NonNull로
    null 인자를 허용하지 않아 안정성을 높인다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/ActivityWindow.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/ActivityWindow.java
@@ -1,7 +1,17 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ActivityWindow {
     private List<Activity> activities;
+    public ActivityWindow(@NonNull List<Activity> activities) {
+        this.activities = activities;
+    }
+    public ActivityWindow(@NonNull Activity... activities) {
+        this.activities = new ArrayList<>(Arrays.asList(activities));
+    }
 }
