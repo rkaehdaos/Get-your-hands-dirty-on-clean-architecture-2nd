6a1e33fdc0286aaddccf4c94d54b4ab848b8b8a7
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 03:12:57 +0900

    ✨ feat(core): Money에 인스턴스 plus/minus 메서드 추가
    정적 add/subtract와 동일한 연산을 인스턴스 메서드
    형태로도 사용할 수 있도록 plus, minus를 추가한다.
    호출부에서 체이닝하기 편한 형태로 연산을 표현할 수 있다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Money.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Money.java
@@ -18,6 +18,9 @@ public class Money {
     public static Money add(Money a, Money b) { return new Money(a.amount.add(b.amount)); }
     public static Money subtract(Money a, Money b) { return new Money(a.amount.subtract(b.amount)); }
 
+    public Money plus(Money money) { return new Money(this.amount.add(money.amount));}
+    public Money minus(Money money) { return new Money(this.amount.subtract(money.amount));}
+
     public Money negate() { return new Money(this.amount.negate()); }
 
 }
