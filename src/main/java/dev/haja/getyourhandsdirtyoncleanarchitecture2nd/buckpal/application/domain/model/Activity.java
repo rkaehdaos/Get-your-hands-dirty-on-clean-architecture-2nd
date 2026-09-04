5b9f63604411fa01cc1b0fccde5830ac5e6be6f1
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 03:03:15 +0900

    ♻️ refactor(core): Activity에 명시적 생성자 도입
    Lombok @RequiredArgsConstructor 대신 id를 제외한 필드를
    받는 명시적 생성자를 작성한다. id는 신규 Activity 생성 시
    아직 할당되지 않은 값이므로 생성자 파라미터에서 제외하고
    null로 초기화한다. @NonNull로 필수 필드의 null 방지를
    유지한다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
@@ -1,20 +1,33 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
 import jakarta.annotation.Nonnull;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
-@RequiredArgsConstructor
 public class Activity {
     ActivityId id;
-    @Nonnull Account.AccountId ownerAccountId;
-    @Nonnull Account.AccountId sourceAccountId;
-    @Nonnull Account.AccountId targetAccountId;
-    @Nonnull LocalDateTime timestamp;
-    @Nonnull Money money;
+    Account.AccountId ownerAccountId;
+    Account.AccountId sourceAccountId;
+    Account.AccountId targetAccountId;
+    LocalDateTime timestamp;
+    Money money;
 
+    public Activity(
+            @NonNull Account.AccountId ownerAccountId,
+            @NonNull Account.AccountId sourceAccountId,
+            @NonNull Account.AccountId targetAccountId,
+            @NonNull LocalDateTime timestamp,
+            @NonNull Money money) {
+        this.id = null;
+        this.ownerAccountId = ownerAccountId;
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.timestamp = timestamp;
+        this.money = money;
+    }
 
     @Value
     public static class ActivityId {
