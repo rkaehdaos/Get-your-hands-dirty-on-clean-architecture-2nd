2e39f9d22c83335c2f92a561acecd694dcacb69b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 02:41:37 +0900

    💎 improve(core): Money를 불변 값 객체로 전환
    @Value 어노테이션을 적용해 Money를 불변 값 객체로 만든다.
    금액을 표현하는 도메인 모델은 생성 이후 변경되지 않아야
    하므로, equals/hashCode/getter를 자동 생성해 값 비교와
    불변성을 보장한다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Money.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Money.java
@@ -1,9 +1,10 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
 import lombok.NonNull;
+import lombok.Value;
 
 import java.math.BigInteger;
-
+@Value
 public class Money {
     @NonNull BigInteger amount;
 }
