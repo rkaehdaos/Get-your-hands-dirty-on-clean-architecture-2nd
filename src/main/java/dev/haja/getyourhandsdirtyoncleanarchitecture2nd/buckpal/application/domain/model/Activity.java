fc1f4b51f979ac2a9586e0e1aa2b8089270f7543
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 02:58:46 +0900

    ✨ feat(core): Activity에 ownerAccountId 필드 추가
    Activity가 속한 계좌를 식별할 수 있도록 ownerAccountId
    필드를 추가하고, @RequiredArgsConstructor로 final이 아닌
    필드를 포함한 생성자를 자동 생성한다.
    
    See also: #2

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Activity.java
@@ -1,10 +1,12 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
-
+@RequiredArgsConstructor
 public class Activity {
     ActivityId id;
-
+    @Nonnull Account.AccountId ownerAccountId;
 
     @Value
     public static class ActivityId {
