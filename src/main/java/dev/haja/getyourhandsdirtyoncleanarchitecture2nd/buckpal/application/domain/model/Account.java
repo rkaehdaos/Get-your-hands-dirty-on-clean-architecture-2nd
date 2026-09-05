1d4e0d0610f593d187ccd7bcf5c4311950b2da2f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 23:13:59 +0900

    ✨ feat(core): Account에 잔액 계산 메서드 추가
    계좌 잔액을 계산하는 calculateBalance 메서드를 추가한다.
    활동 내역 기반 계산 로직은 아직 구현 전이라 우선 더미 값을
    반환하도록 한다.
    
    Refs: #6

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Account.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/Account.java
@@ -1,9 +1,17 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model;
 
+import java.math.BigInteger;
+
 public class Account {
     private AccountId id;
     private Money baselineBalance;
     private ActivityWindow activityWindow;
+
+
+    public Money calculateBalance(){
+        // TODO: calculate Balance, 현재는 더미 데이터 반환
+        return new Money (BigInteger.ZERO);
+    }
 
     // TODO: 비즈니스 룰 검증: 원본(출금) 계좌의 잔액이 마이너스가 되어서는 안 된다
     public boolean withdraw(Money money, AccountId targetAccountId) {
