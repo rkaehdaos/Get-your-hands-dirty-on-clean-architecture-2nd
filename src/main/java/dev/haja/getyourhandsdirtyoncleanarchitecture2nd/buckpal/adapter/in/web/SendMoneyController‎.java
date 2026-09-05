c5fca0b0074114e6f5f85a122eb5da3e265a771b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-06 00:31:57 +0900

    ✨ feat(api): SendMoneyController에 SendMoneyUseCase 의존성 주입
    SendMoneyController가 SendMoneyUseCase를 생성자로 주입받도록
    하고, SendMoneyService를 @Component로 등록해 Spring 빈으로
    주입될 수 있게 한다. 웹 어댑터가 유스케이스를 호출할 수 있는
    기반을 마련한다.
    
    See also: #8

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/in/web/SendMoneyController‎.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/in/web/SendMoneyController‎.java
@@ -1,6 +1,11 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.adapter.in.web;
 
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in.SendMoneyUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class SendMoneyController {}
+@RequiredArgsConstructor
+class SendMoneyController {
+    private final SendMoneyUseCase sendMoneyUseCase;
+}
