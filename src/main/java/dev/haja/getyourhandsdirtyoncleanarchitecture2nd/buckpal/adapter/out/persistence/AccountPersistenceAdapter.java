bb00be0c433170b12a5540ea7bad1e889e7060ed
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-06 02:21:09 +0900

    ✨ feat(db): AccountPersistenceAdapter가 LoadAccountPort 구현하도록 변경
    - LoadAccountPort를 구현하도록 클래스 시그니처 변경
    - loadAccount(AccountId, LocalDateTime) 메서드 시그니처 작성
      (본문 구현은 이후 커밋에서 채울 예정)
    
    See also: #10

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/out/persistence/AccountPersistenceAdapter.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/out/persistence/AccountPersistenceAdapter.java
@@ -1,11 +1,26 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.adapter.out.persistence;
 
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Account;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Account.AccountId;
+
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.out.LoadAccountPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+
 @Component
 @RequiredArgsConstructor
-class AccountPersistenceAdapter {
+class AccountPersistenceAdapter implements
+        LoadAccountPort {
     private final AccountRepository accountRepository;
     private final ActivityRepository activityRepository;
+
+    @Override
+    public Account loadAccount(
+            AccountId accountId,
+            LocalDateTime baselineDate) {
+        return null;
+    }
 }
