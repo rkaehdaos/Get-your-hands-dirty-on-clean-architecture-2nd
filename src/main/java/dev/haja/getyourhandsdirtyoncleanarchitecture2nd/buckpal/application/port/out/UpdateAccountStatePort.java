63e36d44868e8cb1c50494d510467835d5256dc3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 18:11:57 +0900

    ✨ feat(core): UpdateAccountStatePort에 updateActivities 추가
    계좌의 액티비티 변경 사항을 영속화하기 위한 updateActivities
    메서드를 아웃바운드 포트에 정의한다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/out/UpdateAccountStatePort.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/out/UpdateAccountStatePort.java
@@ -1,4 +1,7 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.out;
 
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Account;
+
 public interface UpdateAccountStatePort {
+    void updateActivities(Account account);
 }
