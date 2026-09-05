b2f675d946c29e9a0145512744c108cf063f2602
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 23:14:26 +0900

    ✨ feat(core): GetAccountBalanceService에서 유스케이스 구현
    GetAccountBalanceUseCase를 구현하여, LoadAccountPort로
    현재 시점까지의 계좌 정보를 조회한 뒤 잔액을 계산해 반환한다.
    
    Refs: #6

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/service/GetAccountBalanceService.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/service/GetAccountBalanceService.java
@@ -1,9 +1,18 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.service;
 
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Money;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in.GetAccountBalanceUseCase;
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.out.LoadAccountPort;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
-public class GetAccountBalanceService {
+public class GetAccountBalanceService implements GetAccountBalanceUseCase {
     private final LoadAccountPort loadAccountPort;
+
+    @Override
+    public Money getAccountBalance(GetAccountBalanceQuery query) {
+        return loadAccountPort.loadAccount(query.accountId(), LocalDateTime.now()).calculateBalance();
+    }
 }
