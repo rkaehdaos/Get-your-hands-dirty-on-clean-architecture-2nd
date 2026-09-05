46c7dcce41cdaae38f43893ad60c605fd00dd24b
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 20:08:51 +0900

    💎 improve(core): PositiveMoneyValidator에 null 아님 명시
    isValid의 value 파라미터에 @NotNull을 표기하여 null이
    전달되지 않음을 명시적으로 드러낸다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoneyValidator.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoneyValidator.java
@@ -3,10 +3,11 @@ package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.por
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Money;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.constraints.NotNull;
 
 public class PositiveMoneyValidator implements ConstraintValidator<PositiveMoney, Money> {
     @Override
-    public boolean isValid(Money value, ConstraintValidatorContext context) {
+    public boolean isValid(@NotNull Money value, ConstraintValidatorContext context) {
         return value.isPositive();
     }
 }
