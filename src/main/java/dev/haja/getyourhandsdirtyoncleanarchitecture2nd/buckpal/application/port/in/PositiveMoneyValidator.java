4ae16f24f2ecd5748e8de7f2736acb9234771361
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 20:27:57 +0900

    ♻️ refactor(core): PositiveMoneyValidator에서 검증 애너테이션 제거
    jakarta의 @NotNull 대신 lombok.NonNull import로 교체하며
    파라미터에 붙였던 널 검증 애너테이션을 제거한다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoneyValidator.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoneyValidator.java
@@ -3,11 +3,11 @@ package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.por
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Money;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 
 public class PositiveMoneyValidator implements ConstraintValidator<PositiveMoney, Money> {
     @Override
-    public boolean isValid(@NotNull Money value, ConstraintValidatorContext context) {
+    public boolean isValid(Money value, ConstraintValidatorContext context) {
         return value.isPositive();
     }
 }
