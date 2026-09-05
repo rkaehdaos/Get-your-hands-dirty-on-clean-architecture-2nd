80a77e2eca0bceb61cec773cb68c26e4a5d3f6b3
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-06 02:02:57 +0900

    💄 style(db): AccountJpaEntity import 정리
    - jakarta.persistence 개별 import를 와일드카드로 통합해
      이후 필드/애노테이션 추가 시 import 관리 부담 완화
    
    See also: #10

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/out/persistence/AccountJpaEntity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/adapter/out/persistence/AccountJpaEntity.java
@@ -1,9 +1,6 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.adapter.out.persistence;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
