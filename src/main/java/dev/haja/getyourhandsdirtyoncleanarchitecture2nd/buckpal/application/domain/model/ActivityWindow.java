6481fdea519354cbff00d141e7ce41fcad88da79
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-06 03:40:04 +0900

    ✨ feat(core): ActivityWindow에 calculateBalance 추가
    - 주어진 계좌 id 기준으로 입금/출금 활동을 필터링해
      입금 합계에서 출금 합계를 뺀 잔액을 계산하는
      calculateBalance 메서드 구현
    - Account#calculateBalance에서 더미 데이터 대신 실제
      활동 내역 기반 잔액을 반환하도록 하기 위한 기반 마련
    
    See also: #10

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/ActivityWindow.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/model/ActivityWindow.java
@@ -5,4 +5,18 @@ import lombok.NonNull;
 import java.util.List;
 
 public record ActivityWindow(
-        @NonNull List<Activity> activities) {}
+        @NonNull List<Activity> activities) {
+    public Money calculateBalance(Account.AccountId accountId) {
+        Money depositBalance = activities.stream()
+                .filter(a -> a.targetAccountId().equals(accountId))
+                .map(Activity::money)
+                .reduce(Money.ZERO, Money::add);
+
+        Money withdrawalBalance = activities.stream()
+                .filter(a -> a.sourceAccountId().equals(accountId))
+                .map(Activity::money)
+                .reduce(Money.ZERO, Money::add);
+
+        return Money.add(depositBalance, withdrawalBalance.negate());
+    }
+}
