839f63458dca97f4a596217013f690750a8b764d
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 18:44:40 +0900

    🔨 build(deps): webmvc 스타터 다시 추가
    REST 컨트롤러 구현에 필요한 웹 계층 지원을 위해
    spring-boot-starter-webmvc 의존성을 validation과 함께
    다시 포함시킨다.
    
    See also: #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories { mavenCentral() }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
