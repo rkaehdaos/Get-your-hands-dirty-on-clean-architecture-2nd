31d97228168a210dff7d05d278c05452fbc09776
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 19:13:32 +0900

    ✨ feat(core): PositiveMoney 애너테이션 적용 대상 제한
    @Target(FIELD)를 지정하여 PositiveMoney 애너테이션이 필드에만
    붙을 수 있도록 제한한다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoney.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoney.java
@@ -1,4 +1,9 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in;
 
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target({FIELD})
 public @interface PositiveMoney {
 }
