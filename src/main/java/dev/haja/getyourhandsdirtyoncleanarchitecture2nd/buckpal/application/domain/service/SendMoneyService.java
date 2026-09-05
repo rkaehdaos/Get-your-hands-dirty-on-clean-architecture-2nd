1ff1ed9f263c8279d4566aa167eec7c16487dec9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 18:15:37 +0900

    ✨ feat(core): SendMoneyService가 SendMoneyUseCase 구현
    SendMoneyService가 SendMoneyUseCase를 구현하도록 하고
    sendMoney 메서드의 골격을 TODO와 함께 작성한다. 이후 비즈니스
    규칙 검증, 도메인 모델 상태 변경, 결과 반환 로직을 순차적으로
    채워 넣을 예정이다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/service/SendMoneyService.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/domain/service/SendMoneyService.java
@@ -1,4 +1,13 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.service;
 
-public class SendMoneyService {
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in.SendMoneyUseCase;
+
+public class SendMoneyService implements SendMoneyUseCase {
+    @Override
+    public boolean sendMoney(SendMoneyUseCase sendMoneyUseCase) {
+        // TODO: validate business rules
+        // TODO: manipulate model state
+        // TODO: return output
+        return false;
+    }
 }
