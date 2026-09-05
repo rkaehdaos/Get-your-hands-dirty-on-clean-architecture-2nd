40abcdfe18f7d79abe581372b78928da9940677f
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 19:12:03 +0900

    ✨ feat(core): PositiveMoney 애너테이션의 검증 로직 구현
    PositiveMoneyValidator를 추가하여 Money.isPositive()를 통해
    송금액이 양수인지 실제로 검증하도록 한다.
    
    See also: #4

---
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoneyValidator.java
@@ -0,0 +1,12 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in;
+
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.domain.model.Money;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PositiveMoneyValidator implements ConstraintValidator<PositiveMoney, Money> {
+    @Override
+    public boolean isValid(Money value, ConstraintValidatorContext context) {
+        return value.isPositive();
+    }
+}
