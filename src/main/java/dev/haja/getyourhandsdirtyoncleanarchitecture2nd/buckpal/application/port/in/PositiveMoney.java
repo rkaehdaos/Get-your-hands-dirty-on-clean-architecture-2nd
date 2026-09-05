b97e6b475dca9d02c9036baf9312a466ef4ca8d9
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-05 19:14:31 +0900

    ✨ feat(core): PositiveMoney 애너테이션에 런타임 보존 정책 추가
    @Retention(RUNTIME)을 지정하여 빈 검증 프레임워크가 런타임에
    PositiveMoney 애너테이션을 읽어 검증기를 실행할 수 있도록 한다.
    
    See also: #4

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoney.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/buckpal/application/port/in/PositiveMoney.java
@@ -1,9 +1,13 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.buckpal.application.port.in;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({FIELD})
+@Retention(RUNTIME)
 public @interface PositiveMoney {
 }
