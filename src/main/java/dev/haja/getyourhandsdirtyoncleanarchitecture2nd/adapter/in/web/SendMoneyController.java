6814c9aa8ef8185d515db52a2306c96d812ec82e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-07 23:54:03 +0900

    ✨ feat(api): SendMoneyController 스켈레톤 추가
    웹 어댑터 패키지에 인바운드 컨트롤러 뼈대를 추가한다.
    아직 요청 매핑과 유스케이스 연동은 없다.
    
    Refs: #7

---
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/adapter/in/web/SendMoneyController.java
@@ -0,0 +1,6 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.adapter.in.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class SendMoneyController {}
