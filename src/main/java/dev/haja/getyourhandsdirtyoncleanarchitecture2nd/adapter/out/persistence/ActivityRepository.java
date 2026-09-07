931e2425a92d32387ed284d28313c34c63192206
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-08 00:29:47 +0900

    ✨ feat(db): ActivityRepository 인터페이스 추가
    Spring Data JPA가 ActivityJpaEntity에 대한 CRUD를 자동
    구현하도록 JpaRepository를 상속하는 리포지토리 인터페이스를
    추가한다. 아직 영속성 어댑터에서 이 리포지토리를 사용하지는
    않는다.
    
    Refs: #9

---
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/adapter/out/persistence/ActivityRepository.java
@@ -0,0 +1,8 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface ActivityRepository extends
+        JpaRepository<ActivityJpaEntity, Long> {
+
+}
