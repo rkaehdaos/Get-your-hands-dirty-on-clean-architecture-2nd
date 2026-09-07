38234678ba751e16bf1d9f1cf1fec5e9cbd30024
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-07 22:03:38 +0900

    ✨ feat(core): PositiveMoney를 PositiveMoneyValidator와 연결
    @Constraint(validatedBy = PositiveMoneyValidator.class)를 지정해
    Bean Validation이 이 애노테이션을 만나면 PositiveMoneyValidator로
    검증을 위임하도록 한다.
    
    Refs: #5

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/port/in/PositiveMoney.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/port/in/PositiveMoney.java
@@ -1,5 +1,7 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.port.in;
 
+import jakarta.validation.Constraint;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -9,5 +11,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({FIELD})
 @Retention(RUNTIME)
+@Constraint(validatedBy = PositiveMoneyValidator.class)
 @Documented
 public @interface PositiveMoney {}
