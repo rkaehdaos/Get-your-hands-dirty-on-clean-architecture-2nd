f55750bab1d2a697e4d41935bb37e900b7924acd
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-07 23:55:09 +0900

    ✨ feat(api): sendMoney 요청 매핑 추가
    SendMoneyController에 송금 엔드포인트의 URL 매핑만 먼저
    추가한다. 경로 변수로 출금 계좌, 입금 계좌, 금액을 받는다.
    아직 유스케이스 연동과 실제 처리 로직은 없다.
    
    Refs: #7

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/adapter/in/web/SendMoneyController.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/adapter/in/web/SendMoneyController.java
@@ -1,6 +1,10 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.adapter.in.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class SendMoneyController {}
+class SendMoneyController {
+    @PostMapping(path = "/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+    void sendMoney(){}
+}
