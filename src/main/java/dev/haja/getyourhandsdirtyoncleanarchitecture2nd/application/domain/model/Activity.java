32e7a5ba0bf0125b1954a4dfa7f29edca862c988
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-07 16:12:00 +0900

    ✨ feat(core): Activity 필드에 null 검증 추가
    id를 제외한 Activity의 필수 필드에 @NonNull 애노테이션을
    적용한다. 계좌 정보, 시각, 금액이 누락된 상태로 활동 객체가
    생성되는 것을 방지한다.
    
    See also: #1

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/model/Activity.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/model/Activity.java
@@ -1,16 +1,17 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model;
 
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Account.AccountId;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
 public record Activity(
         ActivityId id,
-        AccountId ownerAccountId,
-        AccountId sourceAccountId,
-        AccountId targetAccountId,
-        LocalDateTime timestamp,
-        Money money) {
+        @NonNull AccountId ownerAccountId,
+        @NonNull AccountId sourceAccountId,
+        @NonNull AccountId targetAccountId,
+        @NonNull LocalDateTime timestamp,
+        @NonNull Money money) {
 
     /**
      * id 없이 객체를 생성할 때 사용하는 생성자 (id는 null로 초기화)
