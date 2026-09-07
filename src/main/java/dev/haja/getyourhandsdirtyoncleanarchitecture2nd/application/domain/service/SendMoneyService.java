eb849453681e7479272f7bb9d2d2e84e2f322ca6
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-07 23:32:21 +0900

    💎 improve(core): SendMoneyService의 접근 제한자를 package-private으로 변경
    SendMoneyService가 public으로 선언돼 있어 외부 패키지에서
    포트(SendMoneyUseCase)가 아닌 구현체에 직접 의존할 수 있는
    길이 열려 있었다. GetAccountBalanceService와 마찬가지로
    서비스 구현은 도메인 패키지 밖에서 보이지 않아도 되므로
    접근 제한자를 제거해 package-private으로 좁힌다. 이렇게 하면
    호출부가 인바운드 포트 인터페이스에만 의존하도록 강제된다.
    
    Refs: #5

--- src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/service/SendMoneyService.java
+++ src/main/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/service/SendMoneyService.java
@@ -3,7 +3,7 @@ package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.serv
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.port.in.SendMoneyCommand;
 import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.port.in.SendMoneyUseCase;
 
-public class SendMoneyService implements SendMoneyUseCase {
+class SendMoneyService implements SendMoneyUseCase {
     @Override
     public boolean sendMoney(SendMoneyCommand sendMoneyCommand) {
         // TODO: 비즈니스 규칙 검증
