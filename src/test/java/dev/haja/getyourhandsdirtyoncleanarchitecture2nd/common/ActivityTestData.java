e7e5cb1c2475055795a1ef19b521d30fe8ca1225
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-11 14:40:33 +0900

    ✅ test(core): Activity 테스트용 ActivityBuilder 추가
    Refs: #11

---
+++ src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/common/ActivityTestData.java
@@ -0,0 +1,60 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.common;
+
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Account.AccountId;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Activity;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Activity.ActivityId;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Money;
+
+import java.time.LocalDateTime;
+
+public class ActivityTestData {
+    public static class ActivityBuilder {
+        private ActivityId id;
+        private AccountId ownerAccountId;
+        private AccountId sourceAccountId;
+        private AccountId targetAccountId;
+        private LocalDateTime timestamp;
+        private Money money;
+
+        public ActivityBuilder withId(ActivityId id) {
+            this.id = id;
+            return this;
+        }
+
+        public ActivityBuilder withOwnerAccount(AccountId accountId) {
+            this.ownerAccountId = accountId;
+            return this;
+        }
+
+        public ActivityBuilder withSourceAccount(AccountId accountId) {
+            this.sourceAccountId = accountId;
+            return this;
+        }
+
+        public ActivityBuilder withTargetAccount(AccountId accountId) {
+            this.targetAccountId = accountId;
+            return this;
+        }
+
+        public ActivityBuilder withTimestamp(LocalDateTime timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public ActivityBuilder withMoney(Money money) {
+            this.money = money;
+            return this;
+        }
+
+        public Activity build() {
+            return new Activity(
+                    this.id,
+                    this.ownerAccountId,
+                    this.sourceAccountId,
+                    this.targetAccountId,
+                    this.timestamp,
+                    this.money);
+        }
+    }
+
+}
