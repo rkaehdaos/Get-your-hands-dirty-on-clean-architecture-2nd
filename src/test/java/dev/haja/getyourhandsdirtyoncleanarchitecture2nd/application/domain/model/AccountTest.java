4635574ea86ad512051567964bd955fd55e9ae2e
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-11 14:27:32 +0900

    ✅ test(core): AccountTest 스켈레톤 추가
    Refs: #11

---
+++ src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/model/AccountTest.java
@@ -0,0 +1,9 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model;
+
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Account.AccountId;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.BDDMockito.then;
+
+class AccountTest { }
