f1dfca9ba0294b2cb010a0a5caf0badde0a88077
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-11 14:31:12 +0900

    ♻️ refactor(test): AccountBuilder를 AccountTestData로 이동
    Refs: #11

--- src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/model/AccountTest.java
+++ src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/application/domain/model/AccountTest.java
@@ -1,37 +1,9 @@
 package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model;
 
-import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Account.AccountId;
-
 //import static org.assertj.core.api.Assertions.assertThat;
 //import static org.mockito.ArgumentMatchers.any;
 //import static org.mockito.BDDMockito.then;
 
 class AccountTest {
 
-    static class AccountBuilder {
-
-        private AccountId accountId;
-        private Money baselineBalance;
-        private ActivityWindow activityWindow;
-
-        public AccountBuilder withAccountId(AccountId accountId) {
-            this.accountId = accountId;
-            return this;
-        }
-
-        public AccountBuilder withBaselineBalance(Money baselineBalance) {
-            this.baselineBalance = baselineBalance;
-            return this;
-        }
-
-        public AccountBuilder withActivityWindow(ActivityWindow activityWindow) {
-            this.activityWindow = activityWindow;
-            return this;
-        }
-
-        public Account build() {
-            return Account.withId(this.accountId, this.baselineBalance, this.activityWindow);
-        }
-
-    }
 }
---
+++ src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/common/AccountTestData.java
@@ -0,0 +1,34 @@
+package dev.haja.getyourhandsdirtyoncleanarchitecture2nd.common;
+
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Account;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.ActivityWindow;
+import dev.haja.getyourhandsdirtyoncleanarchitecture2nd.application.domain.model.Money;
+
+public class AccountTestData {
+    public static class AccountBuilder {
+
+        private Account.AccountId accountId;
+        private Money baselineBalance;
+        private ActivityWindow activityWindow;
+
+        public AccountBuilder withAccountId(Account.AccountId accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public AccountBuilder withBaselineBalance(Money baselineBalance) {
+            this.baselineBalance = baselineBalance;
+            return this;
+        }
+
+        public AccountBuilder withActivityWindow(ActivityWindow activityWindow) {
+            this.activityWindow = activityWindow;
+            return this;
+        }
+
+        public Account build() {
+            return Account.withId(this.accountId, this.baselineBalance, this.activityWindow);
+        }
+
+    }
+}
