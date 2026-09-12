53fdd1521553bf0ed057a516d0ab09c84757e1ce
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2026-09-12 11:16:49 +0900

    💄 style(test): WebEnvironment 참조를 타입 import로 정리
    SpringBootTest.WebEnvironment.RANDOM_PORT라는 완전정규명 대신
    WebEnvironment를 타입으로 임포트해 애노테이션에서 짧게
    참조하도록 정리한다. RANDOM_PORT를 static import로 감췄던
    이전 방식과 달리 WebEnvironment 타입 자체는 드러나
    가독성과 명확성을 함께 챙긴다.
    
    Refs: #20

--- src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/SendMoneySystemTest.java
+++ src/test/java/dev/haja/getyourhandsdirtyoncleanarchitecture2nd/SendMoneySystemTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,7 +20,7 @@ import java.time.LocalDateTime;
 
 
 @AutoConfigureTestRestTemplate
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SendMoneySystemTest {
 
     @Autowired private TestRestTemplate testRestTemplate;
